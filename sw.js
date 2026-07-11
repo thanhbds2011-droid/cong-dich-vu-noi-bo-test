36535ef31c811e69abc0617832a292bdf193b699
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-07-11 16:35:04 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,14 @@
-const CACHE_NAME = "dich-vu-noi-bo-test-v001";
-const ASSETS = ["./", "index.html", "admin.html", "manifest.json"];
-
+const CACHE_NAME = "cong-dich-vu-noi-bo-test-pro-v1";
+const ASSETS = ["./", "./index.html", "./admin.html", "./manifest.json"];
 self.addEventListener("install", event => {
-  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => null));
+  self.skipWaiting();
 });
-
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))
-      .then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
 });
-
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-  const url = new URL(event.request.url);
-  if (url.href.includes("script.google.com")) return;
-  event.respondWith(
-    fetch(event.request).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => null);
-      return res;
-    }).catch(() => caches.match(event.request).then(res => res || caches.match("index.html")))
-  );
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
